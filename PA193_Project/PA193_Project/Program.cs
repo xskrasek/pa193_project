240725f9928697649c80d534c569b7abb33b0b21
Adam Skrášek <469294@muni.cz>
2021-04-01 13:58:14 +0200

    Project test push
    Hopefuly contains everything

---
+++ PA193_Project/PA193_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PA193_Project
+{
+    class CertParser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
