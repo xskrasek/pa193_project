1a9d16c3d90dcf4c7b0a89983727d67861e40469
Michal Pavúk <michal.pavuk@protonmail.com>
2021-04-03 19:20:47 +0200

    Add ParseResult entity

---
+++ PA193_Project/PA193_Project/ParseResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PA193_Project.Entities
+{
+    class ParseResult
+    {
+        public string Title { get; set; }
+        public Dictionary<string, string[]> Versions { get; set; }
+        public string[][] TableOfContents { get; set; }
+        public Revision[] Revisions { get; set; }
+        public Dictionary<string, string> Bibliography { get; set; }
+
+        public class Revision
+        {
+            string version;
+            string date;
+            string description;
+        }
+    }
+}
