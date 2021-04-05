acdf2319598843fadf56759a34ca9182cb55cfd4
Adam Skrášek <469294@muni.cz>
2021-04-05 10:40:44 +0200

    Commit for sync
    - Add skeletons for HF and versions module
    - Add module loading
    - Change string[] to List<strng>

---
+++ PA193_Project/PA193_Project/Modules/HeaderFooterModule.cs
@@ -0,0 +1,27 @@
+﻿using PA193_Project.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PA193_Project.Modules
+{
+    class HeaderFooterModule : IModule
+    {
+        public void Extract(Document document, ref ParseResult intermmediateResult)
+        {
+            var collections = Regex.Matches(document.FullText, @"");
+
+            if (collections.Count() <= 5 )
+            {
+                Console.WriteLine("Not enough data for header/footer removal");
+            }
+            int i = collections[5].Index;
+
+
+
+        }
+    }
+}
---
+++ PA193_Project/PA193_Project/Modules/VersionsModule.cs
@@ -0,0 +1,32 @@
+﻿using PA193_Project.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA193_Project.Modules
+{
+    class VersionsModule : IModule
+    {
+        public void Extract(Document document, ref ParseResult intermmediateResult)
+        {
+            int index = document.FullText.IndexOf('\n');
+            string firstLine = document.FullText.Substring(0, index);
+            Dictionary<string, List<string>> result = new Dictionary<string, List<string>>
+            {
+                { "HELLO", new List<string>() {"WORLD" } }
+            };
+            intermmediateResult.Versions = result;
+
+            /*Versions to do:
+             * eal
+             * global_platform
+             * java_card
+             * sha
+             * rsa
+             * ecc
+             * des*/
+        }
+    }
+}
