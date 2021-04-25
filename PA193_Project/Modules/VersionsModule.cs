8cebd64595b99b58b39992140a25163662bf2f1b
Adam Skrášek <469294@muni.cz>
2021-04-25 15:08:35 +0200

    Update Versions Module
    - Improve versions parsing capabilities by adding custom regex for each of them

--- PA193_Project/Modules/VersionsModule.cs
+++ PA193_Project/Modules/VersionsModule.cs
@@ -16,19 +16,32 @@ namespace PA193_Project.Modules
             int index = document.FullText.IndexOf('\n');
             string firstLine = document.FullText.Substring(0, index);
             Dictionary<string, HashSet<string>> result = new Dictionary<string, HashSet<string>>();
-            List<string> to_find_behind = new List<string>{ "eal", "global_platfom", "java_card", "sha", "rsa", "ecc", "des" };
+            List<string> to_match = new List<string>();
+            List<string> versions = new List<string> { "eal", "global_platfom", "java_card", "sha", "rsa", "ecc", "des" };
+            string eal = @"EAL(\d|\s\d)(\+)?";
+            string gp = @"Global(\s)?Platform \d(\.\d)*";
+            string jc = @"Java Card \d(\.\d)*";
+            string sha = @"SHA(-|\s)?\d{1,3}";
+            string rsa = @"RSA((\s|-|_)?\d{3,4}(\/\d{3,4})?|-CRT|SignaturePKCS1|SSA-PSS)";
+            string ecc = @"ECC(\s|-)?\d{0,4}?";
+            string des = @"(single|3|Triple|T)?(\s|-)?DES\d?";
 
-            foreach (string version in to_find_behind)
+            to_match.AddRange(new List<string>{eal, gp, jc, sha, rsa, ecc, des});
+            int i = 0;
+            foreach (string pattern in to_match)
             {
-                var matches = Regex.Matches(document.FullText, $"{version.ToUpper()}[0-9]*[-]*[0-9]*");
+
+
+                var matches = Regex.Matches(document.FullText, pattern);
+                //For triple-des
+                if (i == 6)  matches = Regex.Matches(document.FullText, pattern, RegexOptions.IgnoreCase);
                 var matches_string = new HashSet<string>();
                 foreach (Match match in matches)
                 {
                     matches_string.Add(match.Value);
                 }
-
-                result.Add(version, matches_string);
-
+                if (matches_string.Count != 0) result.Add(versions[i], matches_string);
+                i++;
             }
 
             intermmediateResult.Versions = result;
