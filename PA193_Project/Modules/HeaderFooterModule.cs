838f3ab1e81fb15b4a116d9de739a824dbe68242
Adam Skrášek <469294@muni.cz>
2021-04-25 10:39:53 +0200

    HeaderFooterUpdate
    There are some inaccuracies, important thing is, if it is helpful like this, or need more work
    
    - Cover "alternating" headers

--- PA193_Project/Modules/HeaderFooterModule.cs
+++ PA193_Project/Modules/HeaderFooterModule.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-//TODO: Erro handling
+//TODO: Error handling
+//TODO: Right now only tries to detect match on one place, wrap it in a cycle so all different frequent patterns get removed.
 namespace PA193_Project.Modules
 {
     class HeaderFooterModule : IModule
@@ -14,6 +15,9 @@ namespace PA193_Project.Modules
         {
             string FullTextCopy = String.Concat(document.FullText.Where(c => !Char.IsWhiteSpace(c) || c == ''));
 
+            //Reverse text, so search is the same as for header
+            string ReverseFullText = new string(document.FullText.Reverse().ToArray());
+            string ReverseFulltext = ReverseFullText.Replace("\n", "n\\");
             var collections = Regex.Matches(FullTextCopy, @"");
 
             if (collections.Count() <= 5 )
@@ -26,12 +30,10 @@ namespace PA193_Project.Modules
             //Go up (lower index) to find matches
             
             string current = "" + FullTextCopy[i];
-            current = new string(current.Reverse().ToArray());
             int test = 0;
-            //extend the string by one char and check, if this pattern appers enough (quite random percentage) times in the whole document
-            while (Regex.Matches(FullTextCopy, current).Count > (collections.Count *30 / 100))
+            //extend the string by one char and check, if this pattern appears enough (quite random percentage) times in the whole document
+            while (Regex.Matches(FullTextCopy, current).Count > (collections.Count *45 / 100))
             {
-                current = new string(current.Reverse().ToArray());
                 i--;
                 if (i > FullTextCopy.Length - 1)
                 {
@@ -39,35 +41,29 @@ namespace PA193_Project.Modules
                 }
 
                 //jump over digit, if previous character is also a digit, e.g. to match both 9/100 nad 50/100 etc...
-                if (Char.IsDigit(FullTextCopy[i]) && Char.IsDigit(FullTextCopy[i - 1])) continue;
-
-    
-                    switch (FullTextCopy[i])
+                if (Char.IsDigit(FullTextCopy[i]) && Char.IsDigit(FullTextCopy[i - 1]))
                 {
-                    case '(':
-                        current += +FullTextCopy[i] + "\\";
-                        break;
-                    case ')':
-                        current += FullTextCopy[i] + "\\" ;
-                        break;
-                    case Char c when Char.IsDigit(c):
-                        current += "+d\\";
-                        break;
-                    default :
-                        current += FullTextCopy[i];
-                        break;
+                    continue;
                 }
 
-                    
-                current = new string(current.Reverse().ToArray());
+
+                current = FullTextCopy[i] switch
+                {
+                    '(' => "\\" + FullTextCopy[i] + current,
+                    ')' => "\\" + FullTextCopy[i] + current,
+                    Char c when Char.IsDigit(c) => "\\d+" + current,
+                    '.' => "\\." + current,
+                    _ => FullTextCopy[i] + current,
+                };
+
                 test = Regex.Matches(FullTextCopy, current).Count;
+                int bo = collections.Count * 30 / 100;
+                bo = bo;
             }
             //remove the char, tha breaks the margin
-            current = new string(current.Reverse().ToArray());
-            string footer = current.Remove(current.Length - 1);
+            string footer = current.Remove(0,1);
 
-
-            var orig_collections = Regex.Matches(document.FullText, @"");
+            var orig_collections = Regex.Matches(ReverseFullText, @"");
 
             //Reverse, so I dont mess up the indexes by removing text from the start
             foreach (var match in orig_collections.Reverse())
@@ -79,14 +75,14 @@ namespace PA193_Project.Modules
 
 
                 //hopefuly -2
-                for (j = 0; j <= text_index; j++)
+                for (j = 0; j < ReverseFullText.Length - text_index - 2; j++)
                 {
                     //document length check
-                    if (text_index - j == 0) break;
+                    if (text_index + j >= ReverseFullText.Length - 1 || ReverseFullText.Length <j) break;
 
                     //Check for whitespace, so comparison is valid with the non-whitespace version
 
-                    while (Char.IsWhiteSpace(document.FullText[text_index - j]))
+                    while (Char.IsWhiteSpace(ReverseFullText[text_index + j]))
                     {
                         j++;
                         index_diff++;
@@ -98,15 +94,15 @@ namespace PA193_Project.Modules
                         break;
                     }
                     char current_char = footer[j - index_diff + 1];
-                    if (document.FullText[text_index - j] != current_char)
+                    if (ReverseFullText[text_index + j] != current_char)
                     {
 
                         //Number matching case, header has \d+ and real text some numbers
-                        if (Char.IsNumber(document.FullText[text_index - j]) && current_char == '\\' && footer[j - index_diff + 2] == 'd')
+                        if (Char.IsNumber(ReverseFullText[text_index + j]) && current_char == '\\' && footer[j - index_diff + 2] == 'd')
                         {
                             //set header behind \d+ and cycle throuh the number. Both should end behind the number
                             index_diff -= 3;
-                            while (Char.IsNumber(document.FullText[text_index - j]))
+                            while (Char.IsNumber(ReverseFullText[text_index + j]))
                             {
                                 j++;
                                 //lower index diff so comparison for header does not break in case of big numbers
@@ -114,7 +110,7 @@ namespace PA193_Project.Modules
                             }
 
                             //This evens out the difference in case of whitespaces after numbers.
-                            while (Char.IsWhiteSpace(document.FullText[text_index - j]))
+                            while (Char.IsWhiteSpace(ReverseFullText[text_index + j]))
                             {
                                 j++;
                                 //lower index diff so comparison for header does not break in case of big numbers
@@ -125,7 +121,7 @@ namespace PA193_Project.Modules
 
                         }
                         //Handle brackets and maybe others in the future
-                        else if (new[] { '(', ')' }.Contains(document.FullText[text_index - j]) && (current_char == '\\')
+                        else if (new[] { '(', ')' }.Contains(ReverseFullText[text_index + j]) && (current_char == '\\')
                             && (new[] { '(', ')' }.Contains(footer[j - index_diff + 2])))
                         {
                             //move header forward and also full text behind the bracket
@@ -135,7 +131,7 @@ namespace PA193_Project.Modules
                         //In case the text is really different and should not be matched further
                         else
                         {
-                            char a = document.FullText[text_index - j];
+                            char a = ReverseFullText[text_index + j];
                             char b = footer[j - index_diff + 1];
 
                             //As the matching should be at least as long as the header text, take all longer as ok 
@@ -152,13 +148,19 @@ namespace PA193_Project.Modules
                 //If this part of document with the symbol matches header, remove it 
                 if (is_match)
                 {
-                    int p = document.FullText.Length;
+                    int p = ReverseFullText.Length;
                     if (j > 0)
                     {
-                        document.FullText = document.FullText.Remove(text_index + 1-j, j - 1);
+
+                        //no +1 here, we need to keep the symbol alive
+                        ReverseFullText = ReverseFullText.Remove(text_index+1, j-2 );
+                        //TODO: This does not "refresh" only last change lives
                     }
                 }
             }
+
+            string ReverseBack = new string(ReverseFullText.Reverse().ToArray());
+            document.FullText = ReverseBack.Replace("n\\", "\n");
             //Console.WriteLine(document.FullText);
 
 
@@ -174,14 +176,19 @@ namespace PA193_Project.Modules
 
 
 
-        //Go down (higher index) to find matches
-        i = collections[5].Index;
+            //Go down (higher index) to find matches
+            i = collections[5].Index;
 
 
-            current = "" + FullTextCopy[i];
-            test = 0;
-            //extend the string by one char and check, if this pattern appers enough (quite random percentage) times in the whole document
-            while (Regex.Matches(FullTextCopy, current).Count > ((collections.Count * 30 ) / 100))
+            int matchCount = 0;
+            while (matchCount < 2) {
+                current = "" + "\f";
+
+                //move to another possible pattern
+                if (matchCount == 1) i = collections[6].Index;
+
+                //extend the string by one char and check, if this pattern appers enough (quite random percentage) times in the whole document
+                while (Regex.Matches(FullTextCopy, current).Count > (collections.Count * 45 / 100))
             {
                 int al = Regex.Matches(FullTextCopy, current).Count;
                 int mon = (collections.Count / 100) *30;
@@ -202,7 +209,7 @@ namespace PA193_Project.Modules
                     case ')':
                         current += "\\" + FullTextCopy[i];
                         break;
-                    case Char c when Char.IsDigit(c):
+                    case char c when Char.IsDigit(c):
                         current += "\\d+";
                         break;
                     default :
@@ -215,6 +222,11 @@ namespace PA193_Project.Modules
             }
              string header = current.Remove(current.Length -1);
 
+             if (header.Length < 6) continue;
+
+
+             matchCount++;
+            
 
             orig_collections = Regex.Matches(document.FullText, @"");
 
@@ -302,13 +314,13 @@ namespace PA193_Project.Modules
                     int p = document.FullText.Length;
                     if (j > 0)
                     {
-                        document.FullText = document.FullText.Remove(text_index + 1, j - 1);
+                        document.FullText = document.FullText.Remove(text_index + 1, j-2);
                     }
                 }
             }
-            //Console.WriteLine(document.FullText);
+           
         }
-
-
+            Console.WriteLine(document.FullText);
+        }
     }
 }
